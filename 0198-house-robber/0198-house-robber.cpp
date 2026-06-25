a0491c59742cc12d9d0ff843b86b420b0ee1e550
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-06-25 12:44:09 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,13 +6,15 @@ public:
 
         if (n == 0) return 0;
         if (n == 1) return nums[0];
-        
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+
+       int prev2 = nums[0];
+       int prev = max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++){
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+          int curi = max(nums[i] + prev2, prev);
+          prev2 = prev;
+          prev = curi;
         }
-        return dp[n-1];
+        return prev;
     }
 };
