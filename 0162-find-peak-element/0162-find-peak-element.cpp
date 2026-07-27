06059420d5003f0ce9039d0e4fc95ccbf1380d09
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-07-28 02:34:40 +0530

    Time: 0 ms (100%), Space: 12.7 MB (40.98%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,8 +1,9 @@
+/*
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        //edge case start
+       //edge case start
        if(n == 1) return 0;
        else if(nums[0] > nums[1]) return 0;
        else if(nums[n-1] > nums[n-2]) return n-1;
@@ -10,12 +11,32 @@ public:
 
        else{
        for(int i = 1; i < nums.size()-1; i++){
-        if(nums[i] > nums[i+1] && nums[i] > nums[i-1]){
+        if(nums[i] > nums[i+1] && nums[i] > nums[i-1])
+        {
             return i;   
-       }
+        }
        }
        }
        
        return 0;
+    }
+};
+*/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] < nums[mid+1])// Slope goes UP to the right -> Walk right
+            {
+                low = mid+1;
+            }
+            else high = mid;
+        }
+        return low;
     }
 };
