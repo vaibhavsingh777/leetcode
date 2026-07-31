2507f785a069ebadc9bd3303f0bbd10214781a67
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-01 04:08:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.22%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
@@ -32,4 +32,33 @@ public:
         }
         return dp[m-1][n-1];
     }
+};*/
+
+//optimal space complexity
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        if (obstacleGrid[0][0] == 1) return 0;
+
+        vector<long long> dp(n, 0);
+        dp[0] = 1; 
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {                  
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+                
+            }
+        }
+
+        return dp[n - 1];
+    }
 };
+
