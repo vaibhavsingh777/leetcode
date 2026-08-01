de93b4621ec995c05ebee9289e269f249df5a225
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-02 05:17:54 +0530

    Time: 0 ms (100%), Space: 15.6 MB (77.82%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,32 +2,25 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int pivot = -1;
-        
-        // Step 1: Find the first element from right that decreases
-        for(int i = n-2; i>=0;i--){
-            if(nums[i] < nums[i + 1])
+        int pivotIndex = -1;
+
+        for(int i = n - 2; i >=0;i--){
+            if(nums[i] < nums[i+1])
             {
-                pivot = i;
+                pivotIndex = i;
                 break;
             }
-        }
-        //If no pivot is found then the array is in strict descending order,
-        //so just reverse the array
-        if(pivot == -1){
-            reverse(nums.begin(), nums.end());
-            return;
         }
 
-        // Step 2: Find the element from right strictly greater than pivot
-        for (int i = n - 1; i > pivot; i--) {
-            if (nums[i] > nums[pivot]) {
-                swap(nums[i], nums[pivot]);
-                break;
-            }
+        if(pivotIndex != -1){
+            for( int i = n-1; i>=0 ; i--){
+                if(nums[i] > nums[pivotIndex])
+                {
+                    swap(nums[i], nums[pivotIndex]);
+                    break;
+                }
+            }                
         }
-        //reverse starting from pivot
-        reverse(nums.begin() + pivot + 1, nums.end());
-        return;
+        reverse(nums.begin() + pivotIndex+1, nums.end());
     }
 };
