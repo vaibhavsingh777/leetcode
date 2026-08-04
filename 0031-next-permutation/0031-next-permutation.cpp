cddc46caa1febb659fd5484934b096fa5bef1dcd
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-04 08:10:19 +0530

    Time: 0 ms (100%), Space: 15.6 MB (78.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int pivotIndex = -1;
 
-        for(int i = n - 2; i >=0;i--){
+        for(int i = n - 2; i >= 0; i--){
             if(nums[i] < nums[i+1])
             {
                 pivotIndex = i;
@@ -13,14 +13,15 @@ public:
         }
 
         if(pivotIndex != -1){
-            for( int i = n-1; i>=0 ; i--){
-                if(nums[i] > nums[pivotIndex])
-                {
-                    swap(nums[i], nums[pivotIndex]);
-                    break;
-                }
-            }                
+        for(int i = n - 1; i >= 0; i--){
+            if(nums[i] > nums[pivotIndex])
+            {
+                swap(nums[i], nums[pivotIndex]);
+                break;
+            }
         }
-        reverse(nums.begin() + pivotIndex+1, nums.end());
+        }
+        reverse(nums.begin() + pivotIndex + 1, nums.end());
+        
     }
 };
