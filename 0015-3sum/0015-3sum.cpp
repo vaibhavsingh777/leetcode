11a6111f0e91f1a95fcf1827d6e83cfb66d7cc70
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-04 08:24:47 +0530

    Time: 32 ms (99.49%), Space: 29 MB (73.53%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,29 +2,47 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        int n = nums.size();
+        
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+        // We only need to iterate up to n - 2 to leave room for left and right pointers
+        for (int i = 0; i < n - 2; ++i) {
+            
+            // OPTIMIZATION 1: Early termination
+            // If the smallest number is positive, it's impossible to sum to 0
+            if (nums[i] > 0) {
+                break;
+            }
+            
+            // Skip duplicates for our base number 'i'
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             
-            int j = i + 1;
-            int k = nums.size() - 1;
+            int left = i + 1;
+            int right = n - 1;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
+                if (sum < 0) {
+                    ++left;
+                } else if (sum > 0) {
+                    --right;
                 } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Move both pointers inward after finding a valid triplet
+                    ++left;
+                    --right;
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+                    // OPTIMIZATION 2: Skip duplicates for both inner pointers
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        ++left;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        --right;
                     }
                 }
             }
