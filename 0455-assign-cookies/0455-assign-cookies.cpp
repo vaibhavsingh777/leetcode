8f500de3aeadb992ea39a514c404f07f1a8f6e26
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-06 22:59:53 +0530

    Time: 12 ms (29%), Space: 44.9 MB (12.49%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0, j = 0;
+        while(i < s.size() && j < g.size()){
+            if(g[j] <= s[i]) 
+            j++;
+            i++;
+        }
+        return j;
+    } 
+};
