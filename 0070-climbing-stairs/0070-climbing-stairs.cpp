5afae1b2b0e1c948292506b52a7a6317c4c79815
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-29 17:15:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-     if(n < 4) return n;
-
-     int prev = 2;
-     int prev1 = 3;
-
-     for(int i = 3; i < n; i++){
-        int curi = prev + prev1;
-        prev = prev1;
-        prev1 = curi;
-     }
-     return prev1;
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 2;
+ 
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i-1] +  dp[i-2];
+        }
+        return dp[n-1];
     }
 };
