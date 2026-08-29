cd601795d4517f87d9d6ca0fa539324d9c8d3e61
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-29 14:48:09 +0530

    Time: 43 ms (83.09%), Space: 28.9 MB (99.33%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,52 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        
         sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
 
-        // We only need to iterate up to n - 2 to leave room for left and right pointers
-        for (int i = 0; i < n - 2; ++i) {
-            
-            // OPTIMIZATION 1: Early termination
-            // If the smallest number is positive, it's impossible to sum to 0
-            if (nums[i] > 0) {
-                break;
+        for(int i = 0; i < n-2; i++){
+           if(nums[i] > 0) break;
+           if(i > 0 && nums[i] == nums[i-1]) continue;
+           int left = i+1;
+           int right = n-1;
+
+           while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum == 0) {
+            ans.push_back({nums[i], nums[left], nums[right]});
+
+            //move pointers & skip duplicates
+            while(left < right && nums[left] == nums[left+1]) left++;
+            while(left < right && nums[right] == nums[right-1]) right--;
+
+            left++;
+            right--;
             }
-            
-            // Skip duplicates for our base number 'i'
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+            else if(sum < 0)
+            {
+                left++;
             }
-            
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum < 0) {
-                    ++left;
-                } else if (sum > 0) {
-                    --right;
-                } else {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Move both pointers inward after finding a valid triplet
-                    ++left;
-                    --right;
-
-                    // OPTIMIZATION 2: Skip duplicates for both inner pointers
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        ++left;
-                    }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        --right;
-                    }
-                }
+            else{
+                right--;
             }
+           }
         }
-        return res;        
+        return ans;
     }
 };
