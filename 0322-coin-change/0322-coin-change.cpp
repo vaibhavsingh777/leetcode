bab420bb21e97a05470c5d62a674107605766dfa
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-08-31 01:29:12 +0530

    Time: 19 ms (80.91%), Space: 18 MB (73.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,18 @@
 class Solution {
-private:
-int solve(vector<int>& coins, int rem, vector<int>& memo){
-    if(rem < 0) return -1;
-    if(rem == 0) return 0;
-    if(memo[rem] != -2) return memo[rem];
-
-    int min_count = INT_MAX;
-
-    for(int coin : coins){
-        int result = solve(coins, rem - coin, memo);
-        if(result >= 0 && result < min_count){
-            min_count = 1 + result;
-        }
-    }
-    memo[rem] = (min_count == INT_MAX) ? -1 : min_count;
-    return memo[rem];
-}
 public:
     int coinChange(vector<int>& coins, int amount) {
-       vector<int> memo(amount + 1, -2);
-       return solve(coins, amount, memo);
+        vector<int> dp(amount+1, amount+1);
+
+        dp[0] = 0; // 0 coins needed to make amount 0
+
+        for(int i = 1; i <= amount; i++){
+            for(auto coin : coins){
+                if(i - coin>=0)
+                {
+                    dp[i] = min(dp[i], 1+dp[i-coin]);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
