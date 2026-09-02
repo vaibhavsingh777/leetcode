13020a62baa06723b37b403b8cd3e7a037b22d71
Vaibhav Kumar Singh <vaibhavworkiitb@gmail.com>
2026-09-02 15:30:03 +0530

    Time: 3 ms (14.78%), Space: 17.7 MB (60.41%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,14 +6,12 @@ public:
         int ans = nums[0];
 
         for(int i = 1; i < nums.size(); i++){
-            //3 choices, start new prod, 
-            //extend prev: both max and min
             int temp = curmax;
             int temp1 = curmin;
 
-            curmax = max({nums[i], temp * nums[i],  temp1 * nums[i]});
-            curmin = min({nums[i], temp * nums[i],  temp1 * nums[i]});
-            ans = max(curmax, ans);
+            curmax = max({nums[i], temp * nums[i], temp1 * nums[i]});
+            curmin = min({nums[i], temp * nums[i], temp1 * nums[i]});
+            ans = max({curmax, ans, curmin});
         }
         return ans;
     }
